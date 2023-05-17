6fafc573ce64ea376c27c6be9c2c1bac6c230b8f
DavidCGTFG <22.daw.davidcarrillo@fundacionmontessori.com>
2023-05-17 14:35:09 +0200

    s5
    s5

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const axios = require('axios');
+const { Sequelize, DataTypes } = require('sequelize');
 
 const app = express();
 
 app.use(express.json());
 
-const baseURL = 'http://54.81.81.83'; // Reemplaza con la URL base de tu API o servidor
+const sequelize = new Sequelize('supervalues', 'adminputty', 'putty', {
+  host: '54.81.81.83',
+  dialect: 'mysql'
+});
+
+
 
 // Ruta para obtener todos los profesores
 app.get('/profesores', async (req, res) => {
-  try {
     const id_P = req.query.id_P; // Obtener el valor del parámetro id_P de la URL
 
     let query = 'SELECT * FROM profesor'; // Consulta SQL inicial sin filtro
@@ -17,36 +21,31 @@ app.get('/profesores', async (req, res) => {
     if (id_P) {
       query = `SELECT * FROM profesor WHERE id_P >= ${id_P}`; // Construir la consulta con filtro si se proporciona el parámetro id_P
     }
-
-    const response = await axios.get(`${baseURL}/profesores`, {
-      params: { query }
-    });
-
-    res.json(response.data);
+  try {
+const profesores = await sequelize.query(query, { type: Sequelize.QueryTypes.SELECT });
+    res.json(profesores);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error al obtener los profesores' });
   }
 });
 
-// Ruta para insertar un nuevo profesor
 app.post('/profesores', async (req, res) => {
   try {
-    const { nombre_P, email, tipo_P } = req.body;
+    const { nombre_P, email, tipo_P } = req.body[0]; // Obtener los datos del nuevo profesor del cuerpo de la solicitud
 
-    const response = await axios.post(`${baseURL}/profesores`, {
-      nombre_P,
-      email,
-      tipo_P
-    });
+    const resultado = await sequelize.query('INSERT INTO profesor (nombre_P, email, tipo_P) VALUES (?,?,?)', {
+      replacements: [nombre_P, email, tipo_P]
+    }); // Insertar el nuevo profesor en la base de datos
 
-    if (response.status === 201) {
-      res.json({ message: 'Profesor insertado correctamente' });
+    if (resultado[0].affectedRows > 0) {
+      res.status(201).json({ message: 'Profesor insertado correctamente' });
     } else {
+      console.error(error.message)
       res.status(500).json({ error: 'Error al insertar el profesor' });
     }
   } catch (error) {
-    console.error(error);
+    console.error(error.message)
     res.status(500).json({ error: 'Error al insertar el profesor' });
   }
 });
